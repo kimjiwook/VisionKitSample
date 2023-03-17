5e898a5e6914f9177bf29909cfc4eb177e74d77d
Jiwook <xodhks_0113@naver.com>
2023-03-17 16:07:23 +0900

    Create Sample Project

---
+++ Vision001/Vision001/Vision001App.swift
@@ -0,0 +1,17 @@
+//
+//  Vision001App.swift
+//  Vision001
+//
+//  Created by JWMacBook on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct Vision001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

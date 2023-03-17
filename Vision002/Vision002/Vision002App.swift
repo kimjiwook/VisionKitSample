1185aac3d541cb136b35e3a9e916ac644f7585f3
Jiwook <xodhks_0113@naver.com>
2023-03-17 17:43:27 +0900

    [main] - Vision002 테스트 준비

---
+++ Vision002/Vision002/Vision002App.swift
@@ -0,0 +1,17 @@
+//
+//  Vision002App.swift
+//  Vision002
+//
+//  Created by JWMacBook on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct Vision002App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
